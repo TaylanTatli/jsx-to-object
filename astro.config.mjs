f950dc0457e49cbc7ed8738a9216f276cb6f8c99
TaylanTatli <taylantatli90@gmail.com>
2025-06-13 21:11:12 +0300

    feat: Add umami integration to Astro configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    umami({
+      id: "4997ea8f-edf7-485b-aa1d-3ce9af56ca28",
+      endpointUrl: "https://stats.tatli.me",
+    }),
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
